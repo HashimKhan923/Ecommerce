0886c1ec940fdb20b096c1042ebec27f25ebec84
unknown <HashimKhan923>
2023-09-20 00:44:54 +0500

    fefefef

--- resources/views/email/customer_verification.blade.php
+++ resources/views/email/customer_verification.blade.php
@@ -1,28 +1,29 @@
-<!-- <h1>Hello {{$name}}</h1>
-<p>
-    Token: {{ $token }}
-</p> -->
-
-<div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
-  <div style="margin:50px auto;width:70%;padding:20px 0">
-    <div style="border-bottom:1px solid #eee">
-      <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">Dragon Auto Mart</a>
-    </div>
-    <p style="font-size:1.1em">Hi, Mr. {{$name}}</p>
-
-    <p>Welcome to Dragon Auto Mart! We're excited to have you on board for this incredible journey. If you ever have any questions, concerns, or suggestions</p> 
-    
-    <p>don't hesitate to contact us at support@dragonautomart.com.</p>
-
-
-    <p>click on the following Link to complete your email verification procedures.</p>
-    <h5><a href="https://api.dragonautomart.com/api/verification/{{$token}}">https://dragonautomart.com/</a></h5>
-    <p style="font-size:0.9em;">Regards,<br />Dragon Auto Mart</p>
-    <hr style="border:none;border-top:1px solid #eee" />
-    <div style="float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">
-      <!-- <p>Your Brand Inc</p>
-      <p>1600 Amphitheatre Parkway</p>
-      <p>California</p> -->
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Email Verification</title>
+  <!-- Add Bootstrap CSS -->
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+  <div class="container" style="min-width:1000px; overflow:auto; line-height:2">
+    <div class="row justify-content-center" style="margin:50px auto; padding:20px 0">
+      <div class="col-md-8" style="border-bottom:1px solid #eee">
+        <!-- Add your company logo image here -->
+        <img src="https://dragonautomart.com/assets/logo-773651ec.webp" width="200" alt="Company Logo" class="img-fluid">
+        <h1 class="mt-3"><a href="#" style="font-size:1.4em; color: #00466a; text-decoration:none; font-weight:600">Dragon Auto Mart</a></h1>
+        <p class="font-size-18">Hi, Mr. {{$name}}</p>
+        <p>Welcome to Dragon Auto Mart! We're excited to have you on board for this incredible journey. If you ever have any questions, concerns, or suggestions,</p> 
+        <p>don't hesitate to contact us at <a href="mailto:support@dragonautomart.com">support@dragonautomart.com</a>.</p>
+        <p>Click on the following link to complete your email verification procedures:</p>
+        <h5><a href="https://api.dragonautomart.com/api/verification/{{$token}}">https://dragonautomart.com/</a></h5>
+        <p class="font-size-14">Regards,<br />Dragon Auto Mart</p>
+        <hr style="border:none; border-top:1px solid #eee" />
+        <div class="text-right text-muted" style="padding:8px 0; font-size:0.8em; font-weight:300">
+          <!-- Your company address or other information can be added here -->
+        </div>
+      </div>
     </div>
   </div>
-</div>
+</body>
+</html>
--- resources/views/email/seller_verification.blade.php
+++ resources/views/email/seller_verification.blade.php
@@ -1,44 +1,75 @@
-<!-- <h1>Hello {{$name}}</h1>
-<p>
-    Token: {{ $token }}
-</p> -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Email Verification - Dragon Auto Mart</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body {
+            font-family: Helvetica, Arial, sans-serif;
+            min-width: 1000px;
+            overflow: auto;
+            line-height: 1.6;
+        }
 
-<div style="font-family: Helvetica,Arial,sans-serif;min-width:1000px;overflow:auto;line-height:2">
-  <div style="margin:50px auto;width:70%;padding:20px 0">
-    <div style="border-bottom:1px solid #eee">
-      <a href="" style="font-size:1.4em;color: #00466a;text-decoration:none;font-weight:600">Dragon Auto Mart</a>
+        .container {
+            margin: 50px auto;
+            width: 70%;
+            padding: 20px 0;
+        }
+
+        .header {
+            border-bottom: 1px solid #eee;
+            text-align: center;
+        }
+
+        .header a {
+            font-size: 1.4em;
+            color: #00466a;
+            text-decoration: none;
+            font-weight: 600;
+        }
+
+        .content {
+            font-size: 1.1em;
+        }
+
+        .content p {
+            margin-bottom: 10px;
+        }
+
+        .cta-button {
+            display: inline-block;
+            padding: 10px 20px;
+            background-color: #00466a;
+            color: #fff;
+            text-decoration: none;
+            border-radius: 5px;
+        }
+
+        .signature {
+            font-weight: bold;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <div class="header">
+            <a href="#"><img src="https://dragonautomart.com/assets/logo-773651ec.webp" width="200" alt="Dragon Auto Mart Logo"></a>
+        </div>
+        <div class="content mt-5">
+            <p>Welcome to Dragon Auto Mart! We're absolutely thrilled to have you join us on this exciting journey.</p>
+            <p>We're excited to let you know that your first 6-month subscription is completely free and on us. It's our way of showing appreciation for your decision to be a part of our community.</p>
+            <p>To get started and learn more about how to make the most of your membership, we encourage you to explore our Sellers FAQ section. You'll find valuable information that will help you navigate your experience with Dragon Auto Mart.</p>
+            <p>We're here to support you every step of the way. If you ever have any questions, concerns, or suggestions, please don't hesitate to reach out to us at <a href="mailto:support@dragonautomart.com">support@dragonautomart.com</a>. Our dedicated support team is ready to assist you.</p>
+            <p>Once again, thank you for choosing Dragon Auto Mart. We can't wait to see your journey unfold on our platform.</p>
+            <p>Best regards,</p>
+            <p class="signature">The Dragon Auto Mart Team</p>
+        </div>
+        <div class="cta-button text-center">
+            <a href="https://api.dragonautomart.com/api/verification/{{$token}}" class="text-white">Click Here Complete Email Verification</a>
+        </div>
     </div>
-    <p style="font-size:1.1em">Dear {{$name}}</p>
-
-    <p>Welcome to Dragon Auto Mart! We're absolutely thrilled to have you join us on this exciting journey.</p>
-
-    <p>We're excited to let you know that your first 6-month subscription is completely free and on us.</p>   
-    <p>It's our way of showing appreciation for your decision to be a part of our community.</p>
-
-    <p>To get started and learn more about how to make the most of your membership, we encourage</p>
-    <p>you to explore our Sellers FAQ section. You'll find valuable information that will help you</p>
-    <p>navigate your experience with Dragon Auto Mart.</p>
-
-    <p>We're here to support you every step of the way. If you ever have any questions, concerns, or</p>
-    <p>suggestions, please don't hesitate to reach out to us at support@dragonautomart.com. Our</p>
-    <p>dedicated support team is ready to assist you.</p>
-
-
-    <p>Once again, thank you for choosing Dragon Auto Mart. We can't wait to see your journey</p>
-    <p>unfold on our platform.</p>
-
-    <p>Best regards,</p>
-    <b>The Dragon Auto Mart Team</b> 
-
-
-    <p>click on the following Link to complete your email verification procedures.</p>
-    <h5><a href="https://api.dragonautomart.com/api/verification/{{$token}}">https://dragonautomart.com/</a></h5>
-    <p style="font-size:0.9em;">Regards,<br />Dragon Auto Mart</p>
-    <hr style="border:none;border-top:1px solid #eee" />
-    <div style="float:right;padding:8px 0;color:#aaa;font-size:0.8em;line-height:1;font-weight:300">
-      <!-- <p>Your Brand Inc</p>
-      <p>1600 Amphitheatre Parkway</p>
-      <p>California</p> -->
-    </div>
-  </div>
-</div>
+</body>
+</html>
