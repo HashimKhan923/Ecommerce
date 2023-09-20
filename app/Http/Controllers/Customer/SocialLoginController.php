d2b6e4f344b3b20ccdcbfdd6ea961ef5a021c889
unknown <HashimKhan923>
2023-09-20 23:34:23 +0500

    jhjhsddfdj

--- app/Http/Controllers/Customer/SocialLoginController.php
+++ app/Http/Controllers/Customer/SocialLoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Customer;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Laravel\Socialite\Facades\Socialite;
+use App\Models\User;
 
 class SocialLoginController extends Controller
 {
