4774cdfcb2b1cb22e9238320f572665be9c117a6
unknown <HashimKhan923>
2023-09-22 01:15:04 +0500

    dwdw

--- app/Http/Controllers/Customer/HomeController.php
+++ app/Http/Controllers/Customer/HomeController.php
@@ -8,11 +8,26 @@ use App\Models\Product;
 use App\Models\Category;
 use App\Models\Brand;
 use App\Models\Banner;
+use Mail;
 
 class HomeController extends Controller
 {
     public function index()
     {
+
+        Mail::send(
+            'email.order_information',
+            [
+                'buyer_name' => $user->name,
+                // 'last_name' => $query->last_name
+            ],
+            function ($message) use ($user) { // Add $user variable here
+                $message->from(env('MAIL_USERNAME'));
+                $message->to($user->email);
+                $message->subject('Order Confirmation');
+            }
+        );
+
         $Products = Product::with('user','category','brand','model','stock','varient','discount','tax','shipping','deal.deal_product','wholesale','shop')->where('published',1)->get();
         $Categories = Category::where('is_active',1)->get();
         $Brands = Brand::with('model')->where('is_active',1)->get();
