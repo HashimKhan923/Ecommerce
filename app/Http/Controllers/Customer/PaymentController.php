0fa5628712be612e5894f93d3d725fe00342b72e
unknown <HashimKhan923>
2023-09-22 23:58:21 +0500

    dwdcejjkkkkkdwswkw

--- app/Http/Controllers/Customer/PaymentController.php
+++ app/Http/Controllers/Customer/PaymentController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Stripe\Stripe;
 use Stripe\Customer;
 use Stripe\Charge;
+use Stripe\PaymentIntent;
 
 class PaymentController extends Controller
 {
@@ -21,24 +22,41 @@ class PaymentController extends Controller
         Stripe::setApiKey(config('services.stripe.secret'));
 
         try {
-            // Create a customer with Stripe
-            $customer = Customer::create([
-                'email' => $token['email'],
-                'source' => $token['id'],
-                'name' => $name,
-            ]);
+            // // Create a customer with Stripe
+            // $customer = Customer::create([
+            //     'email' => $token['email'],
+            //     'source' => $token['id'],
+            //     'name' => $name,
+            // ]);
 
-            // Charge the customer's card
-            $charge = Charge::create([
-                'customer' => $customer->id,
-                'amount' => $amount * 100, // Stripe uses smallest currency unit (cents)
+            // // Charge the customer's card
+            // $charge = Charge::create([
+            //     'customer' => $customer->id,
+            //     'amount' => $amount * 100, // Stripe uses smallest currency unit (cents)
+            //     'currency' => 'usd',
+            //     'description' => 'Dragonautomart-website-payment',
+            //     'receipt_email' => $token['email'],
+            // ]);
+
+            // // Return a JSON response indicating success (HTTP status 201)
+            // return response()->json(['success' => true], 201);
+
+
+            $paymentIntent = PaymentIntent::create([
+                'amount' => $amount * 100,
                 'currency' => 'usd',
-                'description' => 'Dragonautomart-website-payment',
-                'receipt_email' => $token['email'],
+                'statement_descriptor_suffix' => 'Payment using Stripe',
+                'automatic_payment_methods' => [
+                    'enabled' => true,
+                ],
+            ]);
+    
+            return response()->json([
+                'clientSecret' => $paymentIntent->client_secret,
             ]);
 
-            // Return a JSON response indicating success (HTTP status 201)
-            return response()->json(['success' => true], 201);
+
+
         } catch (\Exception $e) {
             // Return a JSON response indicating failure (HTTP status 500)
             return response()->json(['success' => false, 'message' => 'Payment failed'], 500);
