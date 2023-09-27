ff8ffde9f8db4dc0f619ed0b41d2f86ebe7378cd
unknown <HashimKhan923>
2023-09-27 21:21:42 +0500

    dwdwewekjjjsjjded

--- app/Http/Controllers/Customer/ReviewController.php
+++ app/Http/Controllers/Customer/ReviewController.php
@@ -10,6 +10,12 @@ class ReviewController extends Controller
 {
     public function create(Request $request)
     {
+        $check = Review::where('product_id',$request->product_id)->where('user_id',$request->user_id)->first();
+        if($check)
+        {
+            $response = ['status'=>true,"message" => "can't give more than one review on any product!"];
+            return response($response, 200);
+        }
          $new = new Review(); 
          $new->product_id = $request->product_id;
          $new->user_id = $request->user_id;
