d408b98f7f1f77f10862540db0960ed2a5d88d04
unknown <HashimKhan923>
2023-11-02 21:11:00 +0500

    cscdsd

--- app/Http/Controllers/Customer/OrderController.php
+++ app/Http/Controllers/Customer/OrderController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Order;
 use App\Models\Product;
+use App\Models\ProductVarient;
 use App\Models\Stock;
 use App\Models\OrderDetail;
 use Illuminate\Support\Str;
