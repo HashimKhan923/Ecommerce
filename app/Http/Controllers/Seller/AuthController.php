d9905c4a4aa9035838d64add138146e576bdbcf4
unknown <HashimKhan923>
2023-11-07 15:57:16 +0500

    dwds

--- app/Http/Controllers/Seller/AuthController.php
+++ app/Http/Controllers/Seller/AuthController.php
@@ -35,8 +35,7 @@ class AuthController extends Controller
             $new->postal_code = $request->postal_code;
             $new->phone = $request->phone;
             $new->password = Hash::make($request->password);
-            $token = uniqid();
-            $new->remember_token = $token;
+
 
 
         }
@@ -45,6 +44,8 @@ class AuthController extends Controller
             $new = User::where('email',$request->email)->first();
         }
         
+        $token = uniqid();
+        $new->remember_token = $token;
         $new->user_type = 'seller';
         $new->is_active = 1;
         $new->save();
