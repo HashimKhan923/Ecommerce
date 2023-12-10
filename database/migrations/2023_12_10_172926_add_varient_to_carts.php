b644d43535cff3cc6cea99ead924cf5cd5b1a27f
unknown <HashimKhan923>
2023-12-10 22:36:30 +0500

    dsdsd

---
+++ database/migrations/2023_12_10_172926_add_varient_to_carts.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('carts', function (Blueprint $table) {
+            $table->bigInteger('varient_id')->unsigned()->nullable();
+            $table->foreign('varient_id')->references('id')->on('product_varients')->onDelete('cascade')->onUpdate('cascade'); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('carts', function (Blueprint $table) {
+            $table->bigInteger('varient_id');
+        });
+    }
+};
