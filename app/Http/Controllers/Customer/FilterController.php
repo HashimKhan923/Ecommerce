69f2988c813da7cefbddf6568b25aaefbb11c0f1
unknown <HashimKhan923>
2023-12-14 16:28:54 +0500

    dsdsqwsqqwess

--- app/Http/Controllers/Customer/FilterController.php
+++ app/Http/Controllers/Customer/FilterController.php
@@ -33,19 +33,13 @@ class FilterController extends Controller
     public function target_search(Request $request)
     {
         
-        try {
-            $data = Product::with('user', 'category', 'brand', 'model', 'stock', 'product_gallery', 'product_varient', 'discount', 'tax', 'shipping', 'deal.deal_product', 'wholesale')
-                ->where('start_year', '>=', $request->year)
-                ->where('end_year', '<=', $request->year)
-                ->where('brand_id', $request->brand_id)
-                ->where('model_id', $request->model_id)
-                ->get();
-        
-            // Debugging results
-            dd($data);
-        } catch (\Exception $e) {
-            dd($e->getMessage());
-        }
+        $data = Product::with('user','category','brand','model','stock','product_gallery','product_varient','discount','tax','shipping','deal.deal_product','wholesale')   
+        ->where('start_year','>=',2023)
+        ->where('end_year','<=',2023)
+        ->where('brand_id',$request->brand_id)
+        ->where('model_id',$request->model_id)->get();
+
+        return response()->json(['data'=>$data]);
     }
 
     public function multiSearch(Request $request)
