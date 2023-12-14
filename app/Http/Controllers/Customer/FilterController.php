fb6a82101c2734608d04a29605af38cc6b0eb496
unknown <HashimKhan923>
2023-12-14 15:11:25 +0500

    dwddedw

--- app/Http/Controllers/Customer/FilterController.php
+++ app/Http/Controllers/Customer/FilterController.php
@@ -32,6 +32,7 @@ class FilterController extends Controller
 
     public function target_search(Request $request)
     {
+        return $request;
         $data = Product::with('user','category','brand','model','stock','product_gallery','product_varient','discount','tax','shipping','deal.deal_product','wholesale')   
          ->where('start_year','>=',$request->year)
         ->where('end_year','<=',$request->year)
