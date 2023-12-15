ae9cc73c8dee2e964c807210cbfcfc0a3c2021a3
unknown <HashimKhan923>
2023-12-15 12:49:10 +0500

    ewwqwew

--- app/Http/Controllers/Seller/CouponController.php
+++ app/Http/Controllers/Seller/CouponController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Seller;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Coupon;
-use Carbon;
+use Carbon\Carbon;
 
 class CouponController extends Controller
 {
