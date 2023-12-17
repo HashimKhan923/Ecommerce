e82a2285cfa3cb05d31d56db64254efc4316e834
unknown <HashimKhan923>
2023-12-17 19:13:57 +0500

    dwddsddwww

--- app/Http/Controllers/Customer/RefundController.php
+++ app/Http/Controllers/Customer/RefundController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Customer;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Refund;
+use App\Models\Order;
 
 class RefundController extends Controller
 {
@@ -18,13 +19,20 @@ class RefundController extends Controller
     public function create(Request $request)
     {
 
+        $check = Order::where('id',$request->order_id)->first();
+        if($check->delivery_status == 'Delivered')
+        {
+            $response = ['status'=>true,'message'=>'your order has been delivered'];
+            return response($response,201);
+        }
+
         $new = new Refund();
         $new->order_id = $request->order_id;
         $new->seller_id = $request->seller_id;
         $new->reason = $request->reason;
         $new->save();
 
-        $response = ['status'=>true,'message'=>'Refunf request sent successfully!'];
+        $response = ['status'=>true,'message'=>'Refund request sent successfully!'];
         return response($response,200);
 
     }
