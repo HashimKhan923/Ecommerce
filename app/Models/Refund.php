84e81ec1401a517999b409fb5f8e87e2ed9fdc03
unknown <HashimKhan923>
2023-12-17 18:59:52 +0500

    dwddsddwddwdww

--- app/Models/Refund.php
+++ app/Models/Refund.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Refund extends Model
 {
     use HasFactory;
+
+
+    public function order() {
+
+        return $this->belongsTo(Order::class,'order_id','id');
+    }
 }
