fc68fb79ac24d45f13bb603852b2588acf0fe3cc
HashimKhan923 <khanhash1994@gmail.com>
2024-01-11 05:10:10 +0500

    csdsdsd

--- app/Http/Controllers/Customer/PaymentController.php
+++ app/Http/Controllers/Customer/PaymentController.php
@@ -13,33 +13,14 @@ class PaymentController extends Controller
 {
     public function payment(Request $request)
     {
-        // Get the token, amount, and name from the request's input data
         $token = $request->input('token');
         $amount = $request->input('amount');
         $name = $request->input('name');
 
-        // Set the Stripe API secret key from the configuration
         Stripe::setApiKey(config('services.stripe.secret'));
 
         try {
-            // // Create a customer with Stripe
-            // $customer = Customer::create([
-            //     'email' => $token['email'],
-            //     'source' => $token['id'],
-            //     'name' => $name,
-            // ]);
 
-            // // Charge the customer's card
-            // $charge = Charge::create([
-            //     'customer' => $customer->id,
-            //     'amount' => $amount * 100, // Stripe uses smallest currency unit (cents)
-            //     'currency' => 'usd',
-            //     'description' => 'Dragonautomart-website-payment',
-            //     'receipt_email' => $token['email'],
-            // ]);
-
-            // // Return a JSON response indicating success (HTTP status 201)
-            // return response()->json(['success' => true], 201);
 
 
             $paymentIntent = PaymentIntent::create([
