598e149f0b29fb62a411a91ea65709686d9ba7c5
HashimKhan923 <khanhash1994@gmail.com>
2024-01-12 04:41:06 +0500

    sasasaccv

--- app/Http/Controllers/Customer/PaymentController.php
+++ app/Http/Controllers/Customer/PaymentController.php
@@ -4,14 +4,24 @@ namespace App\Http\Controllers\Customer;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+
 use Stripe\Stripe;
 use Stripe\Customer;
 use Stripe\Charge;
 use Stripe\PaymentIntent;
 
+use PayPal\Api\Amount;
+use PayPal\Api\Payer;
+use PayPal\Api\Payment;
+use PayPal\Api\RedirectUrls;
+use PayPal\Api\Transaction;
+use PayPal\Auth\OAuthTokenCredential;
+use PayPal\Rest\ApiContext;
+
+
 class PaymentController extends Controller
 {
-    public function payment(Request $request)
+    public function stripe_payment(Request $request)
     {
         $token = $request->input('token');
         $amount = $request->input('amount');
@@ -48,4 +58,53 @@ class PaymentController extends Controller
             return response()->json(['success' => false, 'message' => 'Payment failed'], 500);
         }
     }
-}
+
+
+    public function paypal_payment(Request $request)
+    {
+        $amount = $request->input('amount');
+
+        $apiContext = new ApiContext(
+            new OAuthTokenCredential(
+                config('services.paypal.client_id'),
+                config('services.paypal.secret')
+            )
+        );
+
+        try {
+            $payer = new Payer();
+            $payer->setPaymentMethod('paypal');
+
+            $amountObj = new Amount();
+            $amountObj->setCurrency('USD')
+                ->setTotal($amount);
+
+            $transaction = new Transaction();
+            $transaction->setAmount($amountObj)
+                ->setDescription('Payment using PayPal');
+
+            $redirectUrls = new RedirectUrls();
+            $redirectUrls->setReturnUrl(url('/paypal/success'))
+                ->setCancelUrl(url('/paypal/cancel'));
+
+            $payment = new Payment();
+            $payment->setIntent('sale')
+                ->setPayer($payer)
+                ->setTransactions([$transaction])
+                ->setRedirectUrls($redirectUrls);
+
+            $payment->create($apiContext);
+
+            $approvalUrl = $payment->getApprovalLink();
+
+            return response()->json([
+                'approvalUrl' => $approvalUrl,
+            ]);
+
+        } catch (\Exception $e) {
+            // Return a JSON response indicating failure (HTTP status 500)
+            return response()->json(['success' => false, 'message' => 'Payment failed'], 500);
+        }
+    }
+
+    }
