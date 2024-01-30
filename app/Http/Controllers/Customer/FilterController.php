107bcca4965777cddc4aa7a446d487fa4783afa0
HashimKhan923 <khanhash1994@gmail.com>
2024-01-30 20:44:09 +0500

    dsdsrweasasswdwdxsasassdwr

--- app/Http/Controllers/Customer/FilterController.php
+++ app/Http/Controllers/Customer/FilterController.php
@@ -25,7 +25,9 @@ class FilterController extends Controller
             $data = Product::with('user', 'category', 'brand', 'model', 'stock', 'product_gallery', 'product_varient', 'discount', 'tax', 'shipping', 'deal.deal_product', 'wholesale')
             ->where(function ($query) use ($request) {
                 $query->where('name', 'LIKE', '%' . $request->searchValue . '%')
-                    ->orWhereJsonContains('tags', 'LIKE', '%' . $request->searchValue . '%');
+                    ->orWhere(function ($query) use ($request) {
+                        $query->whereJsonContains('tags', 'LIKE', '%' . $request->searchValue . '%');
+                    });
             })
             ->get();
         
