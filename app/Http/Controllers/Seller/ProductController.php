42dea3e2230e879448b22f0f47671444defca1db
HashimKhan923 <khanhash1994@gmail.com>
2024-02-01 17:32:22 +0500

    asasdajjd

--- app/Http/Controllers/Seller/ProductController.php
+++ app/Http/Controllers/Seller/ProductController.php
@@ -233,7 +233,6 @@ class ProductController extends Controller
 
     public function update(Request $request)
     {
-        dd($request->photos);
         $update = Product::where('id',$request->id)->first();
         $update->name = $request->name;
         $update->added_by = 'seller';
