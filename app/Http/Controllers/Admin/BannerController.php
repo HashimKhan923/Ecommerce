8f09df3b3eb8bdc80878d3ea768c217b906ea12a
HashimKhan923 <khanhash1994@gmail.com>
2024-02-18 20:18:51 +0500

    sdasasqwqwzaa

--- app/Http/Controllers/Admin/BannerController.php
+++ app/Http/Controllers/Admin/BannerController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Banner;
 use Storage;
+use Intervention\Image\Facades\Image;
+
 
 class BannerController extends Controller
 {
