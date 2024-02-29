2160c6cda81b0a44b98692a78e4be597b43f1d0c
HashimKhan923 <khanhash1994@gmail.com>
2024-02-29 19:48:32 +0500

    dweweesqsqwesdwsqse

---
+++ database/migrations/2024_02_29_143922_add_varient_id_to_order_details_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('order_details', function (Blueprint $table) {
+            $table->renameColumn('varient', 'varient_id');
+            $table->bigInteger('varient_id')->unsigned()->nullable();
+            $table->foreign('varient_id')->references('id')->on('product_varients')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('order_details', function (Blueprint $table) {
+            //
+        });
+    }
+};
