a2d93690e79b65dc9a60f61529ca534f81459763
HashimKhan923 <khanhash1994@gmail.com>
2024-03-06 18:23:36 +0500

    sdsdqs

---
+++ database/migrations/2024_03_06_131945_remove_columns_from_products.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->dropColumn('deal_id');
+            $table->dropColumn('todays_deal');
+            $table->dropColumn('min_qty');
+            $table->dropColumn('rating');
+            $table->dropColumn('end_year');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            //
+        });
+    }
+};
