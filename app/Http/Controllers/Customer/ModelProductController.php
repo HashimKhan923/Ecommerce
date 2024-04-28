5ed9747806090be747556c0d1d4509cc6ded0ec6
HashimKhan923 <khanhash1994@gmail.com>
2024-04-28 11:52:27 +0500

    wqwwdsadqefedwdwsfwskkkqsqwdwssqdssdsxwxswqwdwwdwsqwdwdwdswsq

--- app/Http/Controllers/Customer/ModelProductController.php
+++ app/Http/Controllers/Customer/ModelProductController.php
@@ -8,52 +8,37 @@ use App\Models\Product;
 
 class ModelProductController extends Controller
 {
+    private function getProductsByModel($model_id, $length = null)
+    {
+        $query = Product::with([
+            'user', 'category', 'brand', 'model', 'stock',
+            'product_gallery' => function($query) {
+                $query->orderBy('order', 'asc');
+            }, 'discount', 'tax', 'shipping', 'deal.deal_product',
+            'wholesale', 'shop.shop_policy', 'reviews.user', 'product_varient'
+        ])->where('published', 1)
+        ->where('model_id', $model_id)
+        ->orderBy('id', 'desc');
+    
+        if ($length !== null) {
+            $query->skip($length)->take(12);
+        } else {
+            $query->take(24);
+        }
+    
+        return $query->get();
+    }
+    
     public function index($model_id)
     {
-        $data = Product::with([
-            'user',
-            'category',
-            'brand',
-            'model',
-            'stock',
-            'product_gallery' => function($query) {
-                $query->orderBy('order', 'asc');
-            },
-            'discount',
-            'tax',
-            'shipping',
-            'deal.deal_product',
-            'wholesale',
-            'shop.shop_policy',
-            'reviews.user',
-            'product_varient'
-        ])->where('published',1)->orderBy('id', 'desc')->where('model_id',$model_id)->take(24)->get();
-
-        return response()->json(['data'=>$data]);
+        $data = $this->getProductsByModel($model_id);
+        return response()->json(['data' => $data]);
     }
-
-
+    
     public function load_more($model_id, $length)
     {
-        $data = Product::with([
-            'user',
-            'category',
-            'brand',
-            'model',
-            'stock',
-            'product_gallery' => function($query) {
-                $query->orderBy('order', 'asc');
-            },
-            'discount',
-            'tax',
-            'shipping',
-            'deal.deal_product',
-            'wholesale',
-            'shop.shop_policy',
-            'reviews.user',
-            'product_varient'
-        ])->where('published',1)->orderBy('id', 'desc')->where('model_id',$model_id)->skip($length)->take(12)->get();
-
-        return response()->json(['data'=>$data]);
+        $data = $this->getProductsByModel($model_id, $length);
+        return response()->json(['data' => $data]);
     }
+    
 }
