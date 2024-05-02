6ce0ecbcbbc27b2f579fa63f15df05c019cbe7b6
HashimKhan923 <khanhash1994@gmail.com>
2024-05-02 19:21:03 +0500

    wqwwdsadqefedwdwwdwsssfdefswe

---
+++ app/Console/Commands/CouponUpdate.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\Coupon;
+
+class CouponUpdate extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'coupon:update';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'coupon update successfully!';
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $coupons = Coupon::where('end_date', '<=', now())
+        ->get();
+
+        if($coupons->isNotEmpty())
+        {
+            Coupon::where('end_date', '<=', now())->update(['status' => 0]);
+
+        }
+    }
+}
