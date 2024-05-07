fd0fe9f62bd9f3cb1aedbb9fe2ec0047239baab1
HashimKhan923 <khanhash1994@gmail.com>
2024-05-07 21:21:14 +0500

    wqwwdswdwddwdwdwesassswdwwswwxsxcsqsxxsqszwsxsxcgwdwdd

---
+++ app/Listeners/LogEventListener.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Log\Events\MessageLogged;
+
+
+class LogEventListener
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle(object $event): void
+    {
+        if ($event->level === 'error') {
+            $error = $event->message;
+            // Send an email notification
+            Mail::send('email.error',
+                [
+                    'error' => $error
+                ],
+                function ($message) {
+                    $message->from('support@dragonautomart.com', 'Dragon Auto Mart');
+                    $message->to('support@dragonautomart.com', 'Dragon Auto Mart');
+                    $message->subject('Error Log Notification');
+                }
+            );
+        }
+    }
+}
--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -18,6 +18,9 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        MessageLogged::class => [
+            'App\Listeners\LogEventListener',
+        ]
     ];
 
     /**
