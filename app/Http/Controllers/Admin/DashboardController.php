007ddfdbfc89e9f72b19f0d130995019dffa729f
HashimKhan923 <khanhash1994@gmail.com>
2024-05-08 15:50:03 +0500

    wqwwdswdwddwdwdwesassswdwwwd

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -9,15 +9,20 @@ use App\Models\Product;
 use App\Models\Order;
 use App\Models\User;
 use App\Models\Shop;
+use Illuminate\Support\Facades\Log;
+
 
 class DashboardController extends Controller
 {
     public function index()
     {
         $Users = User::all();
-        $Stores = $St; // Intentional error
+        $Stores = Shop::count();
         $Products = Product::count();
         $Orders = Order::count();
+
+        Log::error('Intentional error: Stores variable should not be used here');
+
         return response()->json(['Users'=>$Users,'Products'=>$Products,'Orders'=>$Orders,'Stores'=>$Stores]);
     }
 }
