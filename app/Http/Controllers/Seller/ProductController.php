d1f35dc6d36b738518f3688c84dea34cb84c69a5
HashimKhan923 <khanhash1994@gmail.com>
2024-05-13 20:09:54 +0500

    wqwwdswdwddwdwdwesassswdwwwdswwddwdxxwssdwd3edws

--- app/Http/Controllers/Seller/ProductController.php
+++ app/Http/Controllers/Seller/ProductController.php
@@ -491,6 +491,22 @@ class ProductController extends Controller
     public function bulk_update(Request $request)
     {
 
+        $request->validate([
+            'ids' => 'required|array',
+            'name' => 'required|string',
+            'category_id' => 'required|int',
+            'weight' => 'required|string',
+            'make' => 'required|string',
+            'brand_id' => 'required|int',
+            'model_id' => 'required|int',
+            'price' => 'required|string',
+            'cost_price' => 'nullable|string',
+            'shop_id' => 'required|int',
+            'varients' => 'nullable|array',
+            'stock' => 'nullable|array',
+            'shipping_type' => 'nullable|string',
+        ]);
+
         foreach($request->ids as $id)
         {
             $update = Product::where('id',$id)->first();
