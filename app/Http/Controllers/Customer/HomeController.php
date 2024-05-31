6988239cda750be80870e12cd1958ba143baf850
HashimKhan923 <khanhash1994@gmail.com>
2024-05-31 09:22:24 +0500

    dwqdqwejhhsqswdwqewdwedewswqkjhjasaswswqwswedwdewwsqs

--- app/Http/Controllers/Customer/HomeController.php
+++ app/Http/Controllers/Customer/HomeController.php
@@ -33,10 +33,18 @@ class HomeController extends Controller
 })->take(24);
     
         $TopSelling = clone $Products;
-        $TopSelling->orderBy('num_of_sale', 'desc')->take(10);
+        $TopSelling = Product::orderBy('num_of_sale', 'desc') // First, order by sales
+                    ->take(50) // Take more than you need to ensure randomness, e.g., top 50
+                    ->inRandomOrder() // Then, randomize the order
+                    ->take(10) // Finally, take the top 10 from the randomized list
+                    ->get();
     
         $TrendingProducts = clone $Products;
-        $TrendingProducts->orderBy('average_rating', 'desc')->take(10);
+        $TrendingProducts->orderBy('average_rating', 'desc')
+        ->take(50) // Take more than you need to ensure randomness, e.g., top 50
+        ->inRandomOrder() // Then, randomize the order
+        ->take(10) // Finally, take the top 10 from the randomized list
+        ->get();
     
         $FeaturedProducts = clone $Products;
         $FeaturedProducts->where('featured', 1)->take(10);
