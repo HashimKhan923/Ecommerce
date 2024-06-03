1da6f108980ae535fbe204f33e6bb36d4eb1737d
HashimKhan923 <khanhash1994@gmail.com>
2024-06-03 16:51:40 +0500

    dwqdqwejhhsqswsdsa

---
+++ database/migrations/2024_06_03_114903_add_sub_category_id_to_products_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            $table->bigInteger('sub_category_id')->unsigned()->nullable();
+            $table->foreign('sub_category_id')->references('id')->on('sub_categories')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('products', function (Blueprint $table) {
+            //
+        });
+    }
+};
