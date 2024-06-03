cb94014483beeb7d176edd24bd43faaaeabeadd8
HashimKhan923 <khanhash1994@gmail.com>
2024-06-03 21:58:41 +0500

    dwqdqwejhhsqswsdsadwddwddwdwdcdccscwdwdwdwed

--- app/Http/Controllers/Seller/ProductController.php
+++ app/Http/Controllers/Seller/ProductController.php
@@ -758,15 +758,15 @@ class ProductController extends Controller
                 {
                     return response()->json(['product_id'=>$productData['id']]);
                 }
-                try{
-                if (isset($productData['min_stock']) || $productData['min_stock'] == 0) {
-                    $stock->min_stock = $productData['min_stock'];
-                }
-            }   
-                catch(Exception $e)
-                {
-                    return response()->json(['product_id'=>$productData['id']]);
-                }
+            //     try{
+            //     if (isset($productData['min_stock']) || $productData['min_stock'] == 0) {
+            //         $stock->min_stock = $productData['min_stock'];
+            //     }
+            // }   
+            //     catch(Exception $e)
+            //     {
+            //         return response()->json(['product_id'=>$productData['id']]);
+            //     }
                 $stock->save();
 
                 if(!empty($productData['discount']))
