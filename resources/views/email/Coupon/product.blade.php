2ad58dfdcd80e2ba7935428709d9abf00b464df5
HashimKhan923 <khanhash1994@gmail.com>
2024-06-12 16:10:22 +0500

    dwqdqwejhhdsdscswdwdwsswswdwdfefdedrefedwseferf

--- resources/views/email/Coupon/product.blade.php
+++ resources/views/email/Coupon/product.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Exclusive Discount Just for You!</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
     <style>
         body {
             font-family: 'Arial', sans-serif;
