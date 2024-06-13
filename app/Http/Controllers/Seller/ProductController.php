a384f6148f181beefb0e661744b42bea46ebca8a
HashimKhan923 <khanhash1994@gmail.com>
2024-06-13 23:38:26 +0500

    dwdwqsxsxwsdsddedxsdgrgefdcscwdwddedddedcsddwdswddwddwswdwddwdcscw

--- app/Http/Controllers/Seller/ProductController.php
+++ app/Http/Controllers/Seller/ProductController.php
@@ -105,6 +105,7 @@ class ProductController extends Controller
             else
             {
                 $filename = $image;
+                $filename = $filename . '.webp';
             }
 
             
