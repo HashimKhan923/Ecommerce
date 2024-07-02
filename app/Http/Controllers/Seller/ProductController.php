80ef451bd315576449d7b4126cf26799d7fd46d0
HashimKhan923 <khanhash1994@gmail.com>
2024-07-02 17:24:48 +0500

    dwdwqsxssdwsdqzqswswdwedewdefefdwdssfeffedfwdwdwdd

--- app/Http/Controllers/Seller/ProductController.php
+++ app/Http/Controllers/Seller/ProductController.php
@@ -57,7 +57,7 @@ class ProductController extends Controller
             'shop.shop_policy',
             'reviews',
             'product_varient'
-        ])->where('user_id', $userId)->skip($start)->take($length)->get();
+        ])->where('user_id', $userId)->orderBy('id', 'desc')->skip($start)->take($length)->get();
     }
     
     public function load_more($userId, $start, $length)
