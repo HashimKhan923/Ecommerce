04ef849528d384c8429bc664a2b98a3534f1f964
HashimKhan923 <khanhash1994@gmail.com>
2024-07-02 19:46:47 +0500

    dwdwqsxssdwsdqzqswswdweddwdwwderdwd

--- app/Http/Controllers/Customer/FilterController.php
+++ app/Http/Controllers/Customer/FilterController.php
@@ -56,7 +56,7 @@ class FilterController extends Controller
                                 WHEN name LIKE ? THEN 2 
                                 ELSE 3 
                             END', ["%$searchValue%", "%$keywords[0]%"])
-            // ->orderByRaw('featured DESC')
+            ->orderByRaw('featured DESC')
             ->get();
         
     
