721e5226c05a6ee4899d7f2592bb68214ab56a50
HashimKhan923 <khanhash1994@gmail.com>
2024-07-23 16:29:12 +0500

    ceddasdwdwddeddwddrdwedgtrgdwddwdsqwdwdserr

--- app/Http/Controllers/Seller/RefundController.php
+++ app/Http/Controllers/Seller/RefundController.php
@@ -71,36 +71,36 @@ class RefundController extends Controller
             }
         );
 
-        $response = ['status'=>true,'message'=>'Refund request sent successfully!'];
+        $response = ['status'=>true,'message'=>'Refund request sent successfully!','refund_id'=>$new->id];
         return response($response,200);
 
     }
 
-    public function is_approved($refund_id)
-    {
-        $is_approved = Refund::where('id',$refund_id)->first();
-        $order = Order::where('id',$is_approved->order_id)->first();
+    // public function is_approved($refund_id)
+    // {
+    //     $is_approved = Refund::where('id',$refund_id)->first();
+    //     $order = Order::where('id',$is_approved->order_id)->first();
 
-        $notification = new Notification();
-        $notification->customer_id = $order->customer_id;
+    //     $notification = new Notification();
+    //     $notification->customer_id = $order->customer_id;
 
-        if($is_approved->seller_approval == 'Not Approved')
-        {
-            $is_approved->seller_approval = 'Approved';
+    //     if($is_approved->seller_approval == 'Not Approved')
+    //     {
+    //         $is_approved->seller_approval = 'Approved';
 
-            $notification->notification = 'your order #'.$order->id.' refund has been approved by seller';
+    //         $notification->notification = 'your order #'.$order->id.' refund has been approved by seller';
             
-        }
-        else
-        {
-            $is_approved->seller_approval = 'Not Approved';
-            $notification->notification = 'your order #'.$order->id.' refund not approved by seller';
+    //     }
+    //     else
+    //     {
+    //         $is_approved->seller_approval = 'Not Approved';
+    //         $notification->notification = 'your order #'.$order->id.' refund not approved by seller';
 
-        }
+    //     }
 
-        $notification->save();
-        $is_approved->save();
-    }
+    //     $notification->save();
+    //     $is_approved->save();
+    // }
 
     public function stripe_refund(Request $request)
     {
