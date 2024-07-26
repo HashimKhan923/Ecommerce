6b614462021415e2fa71ded187bfef164dbc0205
HashimKhan923 <khanhash1994@gmail.com>
2024-07-26 13:45:04 +0500

    ddwdqwdedkjjdwswsssdwddw

--- app/Http/Controllers/Admin/SellerController.php
+++ app/Http/Controllers/Admin/SellerController.php
@@ -13,7 +13,7 @@ class SellerController extends Controller
 {
     public function index()
     {
-        $Sellers = User::with('my_customers.customer','time_line','seller_order.order_timeline','seller_order.order_refund','stafs','shop.shop_policy','seller_information','SellingPlatforms','SocialPlatforms','BankDetail','CreditCard')->withCount(['shop as product_count' => function ($query) {
+        $Sellers = User::with('my_customers.customer','time_line','seller_order.order_timeline','seller_order.order_refund','stafs','shop.shop_policy','shop.order','seller_information','SellingPlatforms','SocialPlatforms','BankDetail','CreditCard')->withCount(['shop as product_count' => function ($query) {
             $query->withCount('product');
         }])->where('user_type','seller')->get();
 
