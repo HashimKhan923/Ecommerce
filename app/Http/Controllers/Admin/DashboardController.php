3d290252247cddb8d980442ce53c99a10b554648
HashimKhan923 <khanhash1994@gmail.com>
2024-08-01 20:55:34 +0500

    wweqsedwedwdweddwddwddwdwewesaswsdwddw

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -8,6 +8,7 @@ use App\Models\SubscribeUser;
 use App\Models\Product;
 use App\Models\Order;
 use App\Models\User;
+use App\Models\Payout;
 use App\Models\Shop;
 use App\Models\Notification;
 use Illuminate\Support\Facades\Log;
@@ -20,11 +21,12 @@ class DashboardController extends Controller
         $Users = User::with('time_line','order.order_timeline','seller_order.order_timeline')->get();
         $Stores = Shop::count();
         $Products = Product::count();
-        $Orders = Order::count();
+        $Orders = Order::all();
+        $Payouts = Payout::all();
 
         $Notifications = Notification::where('customer_id',null)->get();
 
 
-        return response()->json(['Users'=>$Users,'Products'=>$Products,'Orders'=>$Orders,'Stores'=>$Stores,'Notifications'=>$Notifications]);
+        return response()->json(['Users'=>$Users,'Products'=>$Products,'Orders'=>$Orders,'Stores'=>$Stores,'Notifications'=>$Notifications,'Payouts'=>$Payouts]);
     }
 }
