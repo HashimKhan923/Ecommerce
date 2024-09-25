b4be6b3755c8fafad52319350562dd176834ce57
HashimKhan923 <khanhash1994@gmail.com>
2024-09-25 23:42:13 +0500

    wdwdfgfrferfdswsddedfefcefdewfedweddedefrewdwdswddweddsffrfcefedwceeff

--- app/Http/Controllers/Customer/DealProductController.php
+++ app/Http/Controllers/Customer/DealProductController.php
@@ -15,7 +15,7 @@ class DealProductController extends Controller
             'product_gallery' => function($query) {
                 $query->orderBy('order', 'asc');
             },
-            'discount', 'tax', 'shipping',
+            'discount','deal','tax', 'shipping',
             'shop.shop_policy', 'reviews.user', 'product_varient'
         ])->where('published', 1)->whereHas('stock', function ($query) {
             $query->where('stock', '>', 0);
@@ -34,7 +34,7 @@ class DealProductController extends Controller
             'product_gallery' => function($query) {
                 $query->orderBy('order', 'asc');
             },
-            'discount', 'tax', 'shipping',
+            'discount','deal','tax', 'shipping',
             'shop.shop_policy', 'reviews.user', 'product_varient'
         ])->where('published', 1)->whereHas('stock', function ($query) {
             $query->where('stock', '>', 0);
