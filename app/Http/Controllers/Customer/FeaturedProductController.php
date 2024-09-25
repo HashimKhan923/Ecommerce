5877192cb3c1cb8b9134d641f10f1136a95ba1c1
HashimKhan923 <khanhash1994@gmail.com>
2024-09-25 23:33:37 +0500

    wdwdfgfrferfdswsddedfefcefdewfedweddedefrewdwdswddweddsffrfcefedw

--- app/Http/Controllers/Customer/FeaturedProductController.php
+++ app/Http/Controllers/Customer/FeaturedProductController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Customer;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Product;
+
 
 class FeaturedProductController extends Controller
 {
