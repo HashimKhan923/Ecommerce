4d30617aeedd76a7388fffa85f8b23f067f9e241
HashimKhan923 <khanhash1994@gmail.com>
2024-10-01 23:35:05 +0500

    wdwdfgfrferfdswsddedfefcefdewfuyfjefdwdwdwdwddwd

--- app/Http/Controllers/Customer/HomeController.php
+++ app/Http/Controllers/Customer/HomeController.php
@@ -70,8 +70,7 @@ class HomeController extends Controller
         $Models = Models::where('is_active',1)->get();
         $AllBanners = AllBanner::where('status', 1)->get();
         $Shops = Shop::with('seller', 'shop_policy')->where('status',1)->where('featured', 1)->get();
-        $Deal = Deal::with('deal_shop.shop')
-        ->where('discount_start_date', '<=', now())
+        $Deal = Deal::where('discount_start_date', '<=', now())
         ->where('discount_end_date', '>=', now())
         ->where('status',1)
         ->get();
