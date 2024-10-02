19582150ee9149aa852aa8e79e99088ea94c9353
HashimKhan923 <khanhash1994@gmail.com>
2024-10-02 17:48:49 +0500

    wdwdfgfrferfdswsddedfefcefdewfuyfjefdwdwdsdsdswdwdf3dwdfdfdcefdwedw

--- app/Http/Controllers/Customer/HomeController.php
+++ app/Http/Controllers/Customer/HomeController.php
@@ -21,7 +21,7 @@ class HomeController extends Controller
 {
     public function index()
     {
-        return now();
+        return now()->timezone(config('app.timezone'))->format('Y-m-d H:i:s');
         $FeaturedProducts = Product::with([
             'user', 'category', 'sub_category', 'brand', 'model', 'stock',
             'product_gallery' => function ($query) {
@@ -71,10 +71,9 @@ class HomeController extends Controller
         $Models = Models::where('is_active',1)->get();
         $AllBanners = AllBanner::where('status', 1)->get();
         $Shops = Shop::with('seller', 'shop_policy')->where('status',1)->where('featured', 1)->get();
-        $Deal = Deal::
-        where('discount_start_date', '<=', now())
-        ->where('discount_end_date', '>=', now())
-        ->where('status',1)
+        $Deal = Deal::where('discount_start_date', '<=', now()->timezone(config('app.timezone'))->format('Y-m-d H:i:s'))
+        ->where('discount_end_date', '>=', now()->timezone(config('app.timezone'))->format('Y-m-d H:i:s'))
+        ->where('status', 1)
         ->get();
 
 
