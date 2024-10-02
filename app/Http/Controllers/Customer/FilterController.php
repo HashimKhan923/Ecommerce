1a2ce76214180db0e4a20e0c5f25808badacecae
HashimKhan923 <khanhash1994@gmail.com>
2024-10-02 15:28:15 +0500

    wdwdfgfrferfdswsddedfefcefdewfuyfjefdwdwdsdsdswdwdf3dwddwdwdweddwdxswsededdededewdwd

--- app/Http/Controllers/Customer/FilterController.php
+++ app/Http/Controllers/Customer/FilterController.php
@@ -35,7 +35,7 @@ class FilterController extends Controller
                     $query->where('sku',$keyword)
                     ->orWhere('name', 'LIKE', "%{$keyword}%")
                     // ->orWhere('description', 'LIKE', "%{$keyword}%")
-                    ->orWhereJsonContains(strtolower('tags'), strtolower($searchValue));
+                    ->orWhereJsonContains('tags', $searchValue);
                 });
             }
         })
