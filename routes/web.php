c5dc9add864771083e6eb31f49e1461a70860ed1
HashimKhan923 <khanhash1994@gmail.com>
2024-10-04 23:33:05 +0500

    wdwdfgfrferfdswsddedfefcefdewfwdeweffrfghjhghgdwdewddfsdxwsfdfdffcrwdwdwed

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/testing', function () {
-    return view('email.subscribers_notification');
+    return view('email.subscribers_email');
 });
 
 
