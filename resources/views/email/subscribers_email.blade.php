8e1891510a3e079c7a201f29168afc5d26b499ee
HashimKhan923 <khanhash1994@gmail.com>
2024-10-04 23:52:25 +0500

    wdwdfgfrferfdswsddedfefcefdewfwdeweffrfghjhghgdwdewddfsdxwsfdfdffcrwdwdwededddwced

--- resources/views/email/subscribers_email.blade.php
+++ resources/views/email/subscribers_email.blade.php
@@ -637,7 +637,7 @@ table, td { color: #000000; } #u_body a { color: #0000ee; text-decoration: under
   <tr>
     <td class="v-text-align" style="padding-right: 0px;padding-left: 0px;" align="center">
       
-      <img align="center" border="0" src="{{asset('images/image-5.png')}}" alt="image" title="image" style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;clear: both;display: inline-block !important;border: none;height: auto;float: none;width: 70%;max-width: 133px;" width="133" class="v-src-width v-src-max-width"/>
+      <img align="center" border="0" src="{{asset('images/fordlogo.png')}}" alt="image" title="image" style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;clear: both;display: inline-block !important;border: none;height: auto;float: none;width: 70%;max-width: 133px;" width="133" class="v-src-width v-src-max-width"/>
       
     </td>
   </tr>
