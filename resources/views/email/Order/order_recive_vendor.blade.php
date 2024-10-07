cabe679d2a8c0344b45c272b3516a92c4c08160a
HashimKhan923 <khanhash1994@gmail.com>
2024-10-07 23:28:43 +0500

    wdwdfgfrferfdswdweweqwdswedwhghfewdfweddxwddwddwddqwdwdwdwdwswrfefdwwewdefxasx

--- resources/views/email/Order/order_recive_vendor.blade.php
+++ resources/views/email/Order/order_recive_vendor.blade.php
@@ -144,7 +144,7 @@
                     </tr>
                 @endif
                 @if ($request->tax)
-                    @php $total_amount += floatval($request->tax[2]); @endphp
+                    <!-- @php $total_amount += floatval($request->tax[2]); @endphp -->
                     <tr>
                         <td width="75%" align="left"
                             style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 12px; font-weight: 600; line-height: 24px; padding: 10px; border-top: 3px solid #eeeeee; border-bottom: 3px solid #eeeeee;">
@@ -157,7 +157,7 @@
                     </tr>
                 @endif
                 @if ($request->insurance)
-                    @php $total_amount += floatval($request->insurance[0]); @endphp
+                    <!-- @php $total_amount += floatval($request->insurance[0]); @endphp -->
                     <tr>
                         <td width="75%" align="left"
                             style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 12px; font-weight: 600; line-height: 24px; padding: 10px; border-top: 3px solid #eeeeee; border-bottom: 3px solid #eeeeee;">
@@ -170,7 +170,7 @@
                     </tr>
                 @endif
                 @if ($request->signature)
-                    @php $total_amount += floatval($request->signature[0]); @endphp
+                    <!-- @php $total_amount += floatval($request->signature[0]); @endphp -->
                     <tr>
                         <td width="75%" align="left"
                             style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 12px; font-weight: 600; line-height: 24px; padding: 10px; border-top: 3px solid #eeeeee; border-bottom: 3px solid #eeeeee;">
@@ -183,7 +183,7 @@
                     </tr>
                 @endif
                 @if ($request->coupon_discount)
-                    @php $total_amount += $request->coupon_discount; @endphp
+                    <!-- @php $total_amount += $request->coupon_discount; @endphp -->
                     <tr>
                         <td width="75%" align="left"
                             style="font-family: Open Sans, Helvetica, Arial, sans-serif; font-size: 12px; font-weight: 600; line-height: 24px; padding: 10px; border-top: 3px solid #eeeeee; border-bottom: 3px solid #eeeeee;">
