15f85b0d44d5ad155650ee82a6540a1141e36554
HashimKhan923 <khanhash1994@gmail.com>
2024-10-16 12:29:46 +0500

    wdwdfgfrferfdswdwewewewqwdswswdsdssaasqwsedwesddwwdxwddewdccsdwdwsdwscedddwddwdwddwdsdwedwdw

--- app/Http/Controllers/Customer/FilterController.php
+++ app/Http/Controllers/Customer/FilterController.php
@@ -37,6 +37,13 @@ class FilterController extends Controller
                     // ->orWhere('description', 'LIKE', "%{$keyword}%")
                     ->orWhereJsonContains('tags', $searchValue);
                 });
+
+                                // Add shop name search
+                $query->orWhereHas('shop', function ($query) use ($keywords) {
+                    foreach ($keywords as $keyword) {
+                        $query->where('name', 'LIKE', "%{$keyword}%");
+                    }
+                });
             }
         })
         ->orderBy('featured', 'DESC')
