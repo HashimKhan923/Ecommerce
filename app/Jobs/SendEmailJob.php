81ba2adbde092948c7f5a43052202c4b2d605f57
HashimKhan923 <khanhash1994@gmail.com>
2024-10-24 18:16:09 +0500

    sdwewddwdcdexwdwdwqwqwxssdwdwdfwdw

--- app/Jobs/SendEmailJob.php
+++ app/Jobs/SendEmailJob.php
@@ -17,13 +17,13 @@ class SendEmailJob implements ShouldQueue
 
     protected $user;
     protected $details;
- 
+    protected $batchId;
 
-    public function __construct($user, $details)
+    public function __construct($user, $details, $batchId)
     {
         $this->user = $user;
         $this->details = $details;
-        
+        $this->details = $details;
     }
 
     public function handle()
@@ -33,16 +33,16 @@ class SendEmailJob implements ShouldQueue
             Mail::mailer('no_reply')->to($this->user->email)->send(new SubscribersNotificationMa($this->details));
 
             // Update successful email count
-            // EmailBatch::where('id', $this->batchId)->increment('successful_emails');
-            // EmailBatch::where('id', $this->batchId)->update(['to_id' => $this->user->id]);
+            EmailBatch::where('id', $this->batchId)->increment('successful_emails');
+            EmailBatch::where('id', $this->batchId)->update(['to_id' => $this->user->id]);
 
         } catch (\Exception $e) {
             // Check if the error is spam-related
             if ($e->getMessage() === 'Spam detected') {
-                // EmailBatch::where('id', $this->batchId)->increment('spam_emails');
+                EmailBatch::where('id', $this->batchId)->increment('spam_emails');
             } else {
                 // Increment failed email count
-                // EmailBatch::where('id', $this->batchId)->increment('failed_emails');
+                EmailBatch::where('id', $this->batchId)->increment('failed_emails');
             }
         }
     }
