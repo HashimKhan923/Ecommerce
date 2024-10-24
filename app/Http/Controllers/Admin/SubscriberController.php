eabafa3618b21316267bca3fad128e92db41762b
HashimKhan923 <khanhash1994@gmail.com>
2024-10-24 11:48:47 +0500

    sdwewddwdcdexwdwdddeddeddwfewfwexswddwdwed

--- app/Http/Controllers/Admin/SubscriberController.php
+++ app/Http/Controllers/Admin/SubscriberController.php
@@ -50,7 +50,7 @@ class SubscriberController extends Controller
         $details = $request->only('body');
         $userLimit = $request->input('user_limit'); // Get number of users (e.g., 500)
 
-        $users = Subscriber::where('status',null)->limit($userLimit)->get();
+        $users = Subscriber::where('status',null)->take($userLimit)->get();
 
         return $users;
 
