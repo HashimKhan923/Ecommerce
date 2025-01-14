99dd0d95736261e3345f67705c396bd55d29dbc7
HashimKhan923 <khanhash1994@gmail.com>
2025-01-14 20:38:13 +0500

    eerrwwswssqasdswd

--- app/Http/Controllers/Customer/AuthController.php
+++ app/Http/Controllers/Customer/AuthController.php
@@ -181,7 +181,7 @@ class AuthController extends Controller
     public function social_login(Request $request)
     {
         $check_user = User::where('email',$request->email)->first();
-        
+       return $request->device_token;
         if($request->device_token)
         {
             $check_user->device_token = $request->device_token;
