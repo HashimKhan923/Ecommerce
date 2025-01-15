8b918683d3a7d66d909e75bc0d7ab726fa45707d
HashimKhan923 <khanhash1994@gmail.com>
2025-01-15 20:41:49 +0500

    eerrwwswssqasdswdwssdxsadedswsxw

--- app/Http/Controllers/Customer/HomeController.php
+++ app/Http/Controllers/Customer/HomeController.php
@@ -83,7 +83,7 @@ class HomeController extends Controller
         $Categories = Category::with([
             'subCategories' => function ($query) {
                 $query->withCount('product') 
-                      ->orderBy('order', 'asc'); 
+                      ->orderBy('category_sub_category.order', 'asc'); 
             }
         ])
         ->whereHas('product')
