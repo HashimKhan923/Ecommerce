371da98692ed633decaa06a3d1b8afb7e15d56cf
HashimKhan923 <khanhash1994@gmail.com>
2025-01-15 21:20:35 +0500

    eerrwwswssqasdswdwssdxsadedswsxwsqsxwswdwddw

--- app/Http/Controllers/Customer/ShopProductController.php
+++ app/Http/Controllers/Customer/ShopProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Customer;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
+use Illuminate\Support\Facades\DB;
 
 class ShopProductController extends Controller
 {
