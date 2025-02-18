217bcab40c851656fc687e2862813d7561861ccc
HashimKhan923 <khanhash1994@gmail.com>
2025-02-18 20:20:07 +0500

    dwdweqsadsdjjhggdwdwdwxwsdcedwdwdwdwddwddwd

--- app/Http/Controllers/Seller/OrderController.php
+++ app/Http/Controllers/Seller/OrderController.php
@@ -33,7 +33,7 @@ class OrderController extends Controller
 
     public function detail($id)
     {
-        $paymentIntent = '';
+        $charge = '';
 
         $data = Order::with('order_detail.products.product_gallery','order_detail.products.category','order_detail.products.sub_category','order_detail.products.brand','order_detail.products.model','order_detail.products.stock','order_detail.products.brand','order_detail.products.model','order_detail.products.stock','order_detail.varient','order_detail.products.reviews.user','order_detail.products.tax','order_detail.products.shop.shop_policy','order_status','order_tracking','order_refund','shop','nagative_payout_balance','coupon_user.coupon','order_timeline')->where('id',$id)->first();
         if($data->payment_method == 'STRIPE')
@@ -42,10 +42,9 @@ class OrderController extends Controller
 
 
             try {
-                $paymentIntent = PaymentIntent::retrieve($data->stripe_payment_id); // Use PaymentIntent instead of Charge
+                $paymentIntent = PaymentIntent::retrieve($data->stripe_payment_id);
             
-                // Extract charge details
-                
+                $charge = Charge::retrieve($paymentIntent->latest_charge);                
    
             } catch (\Exception $e) {
                 return response()->json(['error' => $e->getMessage()]);
@@ -53,7 +52,7 @@ class OrderController extends Controller
             
         }
 
-        return response()->json(['data'=>$data,'paymentIntent'=>$paymentIntent]);
+        return response()->json(['data'=>$data,'charge'=>$charge]);
 
     }    
 
