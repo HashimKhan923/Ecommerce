f2846d5c2fc318a3337b2ff4c180516c08c151c4
HashimKhan923 <khanhash1994@gmail.com>
2025-02-26 21:03:59 +0500

    dsdswdwd

--- app/Http/Controllers/Customer/SiteMapController.php
+++ app/Http/Controllers/Customer/SiteMapController.php
@@ -5,13 +5,21 @@ namespace App\Http\Controllers\Customer;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Category;
+use App\Models\SubCategory;
+use App\Models\Model;
+use App\Models\Brand;
 
 class SiteMapController extends Controller
 {
     public function product_ids()
     {
-      $product_ids = Product::pluck('id');
+      $products = Product::pluck('id','name');
+      $categories = Category::pluck('id','name');
+      $sub_categories = SubCategory::pluck('id','name');
+      $models = Model::pluck('id','name');
+      $brands = Brand::pluck('id','name');
 
-      return response()->json(['product_ids'=> $product_ids]);
+      return response()->json(['products'=> $products,'categories'=> $categories,'sub_categories'=> $sub_categories,'models'=> $models,'brands'=> $brands]);
     }
 }
