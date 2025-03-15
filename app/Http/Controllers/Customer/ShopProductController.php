a8802f5d6201e5c1897c8b446fa8df81dd11bdf7
HashimKhan923 <khanhash1994@gmail.com>
2025-03-15 20:57:45 +0500

    dwdwwedxwsedexdwedwdwdexwsdkjhmn

--- app/Http/Controllers/Customer/ShopProductController.php
+++ app/Http/Controllers/Customer/ShopProductController.php
@@ -53,10 +53,7 @@ class ShopProductController extends Controller
     
         // Apply search logic if a search value is provided
         if ($searchValue && !empty($searchValue)) {
-            
-            $stopWords = ['for', 'the', 'a', 'and', 'of', 'to', 'on', 'in'];
-            $searchWords = explode(' ', strtolower($searchValue));
-            $keywords = array_diff($searchWords, $stopWords); // Remove stop words
+            $keywords = explode(' ', $searchValue); // Split the searchValue into keywords
     
             $query->where(function ($query) use ($keywords) {
                 foreach ($keywords as $keyword) {
