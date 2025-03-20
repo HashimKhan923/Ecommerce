5f97725547546e288e9398d7a1c8dacb742f7ac5
HashimKhan923 <khanhash1994@gmail.com>
2025-03-21 01:14:40 +0500

    edwdwdwddefxsddwedcdc

--- app/Http/Controllers/Customer/FilterController.php
+++ app/Http/Controllers/Customer/FilterController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\UserSearchingKeyword;
+use Illuminate\Support\Str;
 
 class FilterController extends Controller
 {
