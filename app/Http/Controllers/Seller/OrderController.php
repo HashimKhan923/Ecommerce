65dc32d6bcf086ba3a305a39f7930b2565ced1df
HashimKhan923 <khanhash1994@gmail.com>
2025-04-21 19:08:20 +0500

    wdwdqwdwdxwdwcedfewd

--- app/Http/Controllers/Seller/OrderController.php
+++ app/Http/Controllers/Seller/OrderController.php
@@ -30,10 +30,9 @@ class OrderController extends Controller
 {
     public function index($id)
     {
-        // $data = Order::with('order_detail.products.product_gallery','order_detail.products.category','order_detail.products.sub_category','order_detail.products.brand','order_detail.products.model','order_detail.products.stock','order_detail.products.brand','order_detail.products.model','order_detail.products.stock','order_detail.varient','order_detail.products.reviews.user','order_detail.products.tax','order_detail.products.shop.shop_policy','order_status','order_tracking','order_refund','shop','nagative_payout_balance','coupon_user.coupon','order_timeline')->where('sellers_id',$id)->get();
-        return response()->json(['message'=>'hello']);
+        $data = Order::with('order_detail.products.product_gallery','order_detail.products.category','order_detail.products.sub_category','order_detail.products.brand','order_detail.products.model','order_detail.products.stock','order_detail.products.brand','order_detail.products.model','order_detail.products.stock','order_detail.varient','order_detail.products.reviews.user','order_detail.products.tax','order_detail.products.shop.shop_policy','order_status','order_tracking','order_refund','shop','nagative_payout_balance','coupon_user.coupon','order_timeline')->where('sellers_id',$id)->get();
 
-        // return response()->json(['data'=>$data]);
+        return response()->json(['data'=>$data]);
     }
 
     public function detail($id)
