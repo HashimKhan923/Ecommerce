89ede440c9d99b0fba271a2d25ba1b66a533fac1
HashimKhan923 <khanhash1994@gmail.com>
2025-04-21 19:34:55 +0500

    wdwdqwdwdxwdwcedfewdedcdcsdw

--- app/Http/Controllers/Seller/PayoutController.php
+++ app/Http/Controllers/Seller/PayoutController.php
@@ -13,7 +13,7 @@ class PayoutController extends Controller
 {
     public function index($id)
     {
-        $data = Payout::with('order.order_detail.products.shop','order.shop','order.nagative_payout_balance','listing_fee','featuredProductOrders','seller')->where('seller_id',$id)->get();
+        $data = Payout::with('order.order_detail.products.shop','listing_fee','featuredProductOrders','seller')->where('seller_id',$id)->get();
 
         return response()->json(['data'=>$data]);
     }
