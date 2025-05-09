e9d1d645f71dfe5cbdf0759e660c8939ff82cafb
HashimKhan923 <khanhash1994@gmail.com>
2025-05-09 15:32:38 +0500

    dwewewe

--- app/Http/Controllers/Customer/HomeController.php
+++ app/Http/Controllers/Customer/HomeController.php
@@ -27,7 +27,7 @@ class HomeController extends Controller
             'product_gallery' => function ($query) {
                 $query->orderBy('order', 'asc');
             },
-            'discount','deal', 'shop', 'reviews.user', 'product_varient'
+            'discount','shop', 'reviews.user', 'product_varient'
         ])->where('published', 1)
         //   ->whereHas('stock', function ($query) {
         //       $query->where('stock', '>', 0);
@@ -39,47 +39,6 @@ class HomeController extends Controller
           ->take(20)
           ->get();
     
-        // $DealProducts = Product::with([
-        //     'stock','wishlistProduct',
-        //     'product_gallery' => function($query) {
-        //         $query->orderBy('order', 'asc');
-        //     },
-        //     'discount','deal',
-        //     'shop', 'reviews.user', 'product_varient'
-        // ])->where('published', 1)
-        // // ->whereHas('stock', function ($query) {
-        // //     $query->where('stock', '>', 0);
-        // // })
-        // ->whereHas('shop', function ($query) {
-        //     $query->where('status', 1);
-        // })->where('deal_id',4)
-        // ->orderByRaw('RAND()') 
-        // ->take(20)
-        // ->get();
-
-
-        // $NewArrivals = Product::with([
-        //    'stock','wishlistProduct',
-        //     'product_gallery' => function ($query) {
-        //         $query->orderBy('order', 'asc');
-        //     },
-        //     'discount','shop.shop_policy', 'reviews.user', 'product_varient'
-        // ])
-        // ->where('published', 1)
-        // // ->whereDate('created_at', '>=', now()->subMonth()) // Filter products from the last month
-        // ->orderBy('created_at', 'desc') // Order by creation date in ascending order
-        // // ->whereHas('stock', function ($query) {
-        // //     $query->where('stock', '>', 0);
-        // // })
-        // ->whereHas('shop', function ($query) {
-        //     $query->where('status', 1);
-        // })
-        // ->orderByRaw('RAND()') 
-        // ->take(20)
-        // ->get();
-
-    
-
         $Categories = Category::with([
             'subCategories' => function ($query) {
                 $query->withCount('product') 
@@ -102,10 +61,68 @@ class HomeController extends Controller
     ->where('status', 1)
     ->withCount('product') 
     ->get();
-        // $Deal = Deal::where('discount_start_date', '<=', now())
-        // ->where('discount_end_date', '>=', now())
-        // ->where('status', 1)
-        // ->get();
+
+
+
+    
+        return response()->json([
+            'FeaturedProducts' => $FeaturedProducts,
+            'Categories' => $Categories,
+            'SubCategories'=>$SubCategories,
+            'Brands' => $Brands,
+            'Models' => $Models,
+            'Banners' => $Banners,
+            'AllBanners' => $AllBanners,
+            'Shops' => $Shops,
+            'States' => $States
+        ]);
+    }
+
+    public function index1()
+    {
+        $FeaturedProducts = Product::with([
+            'stock','wishlistProduct',
+            'product_gallery' => function ($query) {
+                $query->orderBy('order', 'asc');
+            },
+            'discount','shop', 'reviews.user', 'product_varient'
+        ])->where('published', 1)
+        //   ->whereHas('stock', function ($query) {
+        //       $query->where('stock', '>', 0);
+        //   })
+          ->whereHas('shop', function ($query) {
+              $query->where('status', 1);
+          })->where('featured', 1)
+          ->orderByRaw('RAND()') 
+          ->take(20)
+          ->get();
+    
+          $Categories = Category::select('id', 'name','icon','mobile_banner') 
+          ->with([
+              'subCategories' => function ($query) {
+                  $query->select('id', 'name','category_id')
+                        ->withCount('product')
+                        ->orderBy('category_sub_category.order', 'asc');
+              }
+          ])
+          ->whereHas('product')
+          ->where('is_active', 1)
+          ->orderBy('order', 'asc')
+          ->get();
+      
+    
+
+        $Brands = Brand::select('id','name','logo','banner')->with('model')->withCount('product')->whereHas('product')->where('is_active', 1)->orderByDesc('product_count')->get();
+        $Banners = Banner::select('id','mobile_link','mobile_image')->where('status', 1)->get();
+        $States = State::where('status', 1)->get();
+        $SubCategories = SubCategory::select('id', 'name','category_id')->with('category')->where('is_active', 1)->get();
+        $Models = Models::select('id', 'name','logo','banner')->where('is_active',1)->get();
+        $AllBanners = AllBanner::select('id','mobile_link','mobile_image')->where('status', 1)->get();
+        $Shops = Shop::with('seller', 'shop_policy')
+        ->where('status', 1)
+        ->withCount('product') 
+        ->get();
+
 
 
     
--- routes/api.php
+++ routes/api.php
@@ -897,6 +897,7 @@ Route::group(['middleware' => ['auth:api']], function(){
         Route::group(['prefix' => '/home'], function() {
             Route::controller(App\Http\Controllers\Customer\HomeController::class)->group(function () {
                 Route::get('','index');
+                Route::get('mobile','index1');
                 Route::get('load_more_top_selling/{length}','load_more_top_selling');
                 Route::get('load_more_trending/{length}','load_more_trending');
                 Route::get('load_more_featured/{length}','load_more_featured');
