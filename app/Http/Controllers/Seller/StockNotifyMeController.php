b1d7851f53c4c068e1bcb0bee0111614924fb3e4
HashimKhan923 <khanhash1994@gmail.com>
2025-05-12 22:35:36 +0500

    wdwe

--- app/Http/Controllers/Seller/StockNotifyMeController.php
+++ app/Http/Controllers/Seller/StockNotifyMeController.php
@@ -22,66 +22,62 @@ class StockNotifyMeController extends Controller
     public function notify($id)
     {
         $data = StockNotifyMe::find($id);
+        if (!$data) {
+            return response(['status' => false, 'message' => 'Notification data not found.'], 404);
+        }
     
-        
-            if ($data->variant_id) {
-                // Case 1: Notify when the variant is back in stock
-                $variantAvailable = Product::where('id', $data->product_id)
-                    ->whereHas('product_varient', function ($query) use ($data) {
-                        $query->where('id', $data->variant_id)
-                              ->where('stock', '>', 0);
-                    })->exists();
+        if ($data->variant_id) {
+            // Case 1: Variant stock check
+            $variantAvailable = Product::where('id', $data->product_id)
+                ->whereHas('product_varient', function ($query) use ($data) {
+                    $query->where('id', $data->variant_id)->where('stock', '>', 0);
+                })->exists();
     
-                if ($variantAvailable) {
-                    // Send notification for the variant back in stock
-                    $this->notifyCustomer($data);
-                    // Mark as notified
-                    $data->status = 'notified';
-                    $data->save();
-                }
-                else
-                {
-                    return response(['status' => false, "message" => "The Product is out of stock!"], 400);
-                }
-    
+            if ($variantAvailable) {
+                $this->notifyCustomer($data);
+                $data->status = 'notified';
+                $data->save();
             } else {
-                // Case 2: Notify when the main product is back in stock
-                $productAvailable = Product::where('id', $data->product_id)
-                    ->whereHas('stock', function ($query) {
-                    $query->where('stock', '>', 0);
-                    })
-                    ->exists();
-    
-                if ($productAvailable) {
-                    // Send notification for the product back in stock
-                    $this->notifyCustomer($data);
-                    // Mark as notified
-                    $data->status = 'notified';
-                    $data->save();
-                }
-                else
-                {
-                    return response(['status' => false, "message" => "The Product is out of stock!"], 400);
-                }
+                return response(['status' => false, 'message' => 'The Product is out of stock!'], 400);
             }
-        
+    
+        } else {
+            // Case 2: Main product stock check
+            $productAvailable = Product::where('id', $data->product_id)
+                ->whereHas('stock', function ($query) {
+                    $query->where('stock', '>', 0);
+                })->exists();
+    
+            if ($productAvailable) {
+                $this->notifyCustomer($data);
+                $data->status = 'notified';
+                $data->save();
+            } else {
+                return response(['status' => false, 'message' => 'The Product is out of stock!'], 400);
+            }
+        }
     }
-
-
-
+    
     protected function notifyCustomer($data)
     {
-        // Example of sending an email notification to the customer
         $product = Product::find($data->product_id);
-        $variantName = $data->variant_id ? $product->product_varient->find($data->variant_id)->name : null;
-        
-        // Use the ProductBackInStock Mailable to send an email
+        if (!$product) return;
+    
+        $variantName = null;
+        if ($data->variant_id) {
+            $variant = $product->product_varient()->where('id', $data->variant_id)->first();
+            $variantName = $variant ? $variant->name : null;
+        }
+    
+        $image = $product->product_single_gallery->image ?? null;
+    
         Mail::to($data->email)->send(new ProductBackInStock(
             $data->email,
-            $product->product_single_gallery->image,
+            $image,
             $product->name,
             $variantName,
-            'https://dragonautomart.com/product/'.$product->id
+            'https://dragonautomart.com/product/' . $product->id
         ));
     }
+    
 }
