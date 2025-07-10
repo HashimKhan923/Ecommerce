45624689e392afbef2f82ee72f78a07005ebff8d
HashimKhan923 <khanhash1994@gmail.com>
2025-07-10 20:36:51 +0500

    dsdwdwq

--- database/migrations/2025_07_10_103620_add_campaign_id_to_orders_table.php
+++ database/migrations/2025_07_10_103620_add_campaign_id_to_orders_table.php
@@ -12,7 +12,8 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('orders', function (Blueprint $table) {
-            $table->foreignId('campaign_id')->constrained()->onDelete('cascade')->nullable();
+            $table->bigInteger('campaign_id')->unsigned()->nullable();
+            $table->foreign('campaign_id')->references('id')->on('campaigns')->onDelete('cascade')->onUpdate('cascade');
         });
     }
 
