a45f48b98e2632a074d6e4230a847b35340c90e4
HashimKhan923 <khanhash1994@gmail.com>
2025-08-11 14:58:35 +0500

    dweweqweqcsdwxwdxswxwdsdwswdwdxwd ddexwxwedxed

--- app/Http/Controllers/Customer/HomeController.php
+++ app/Http/Controllers/Customer/HomeController.php
@@ -42,7 +42,7 @@ class HomeController extends Controller
 
           $trendingKeywords = AiTrendingProduct::pluck('names')->toArray();
 
-          return $trendingKeywords;
+        //   return $trendingKeywords;
 
             $trendingProducts = collect();
 
