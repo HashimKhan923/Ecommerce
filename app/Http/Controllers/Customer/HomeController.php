5def538514e9f8e1577e44f0d9694c584b437f3d
HashimKhan923 <khanhash1994@gmail.com>
2025-08-12 11:50:24 +0500

    dweweqweqcsdwfedwdweddwddwwewewdsddwdxxcsdcdwdcewddwdedwwdw

--- app/Http/Controllers/Customer/HomeController.php
+++ app/Http/Controllers/Customer/HomeController.php
@@ -51,7 +51,7 @@ $FeaturedProducts = Product::select('id', 'name', 'shop_id', 'price', 'featured'
 $trendingProducts = collect();
 
 foreach ($trendingKeywords as $keyword) {
-    $words = explode(' ', trim($keyword));
+    // $words = explode(' ', trim($keyword));
 
     $matched = Product::select('id', 'name', 'shop_id', 'price', 'featured')
         ->with([
@@ -69,11 +69,8 @@ foreach ($trendingKeywords as $keyword) {
         ])
         ->where('published', 1)
         ->whereHas('shop', fn($q) => $q->where('status', 1))
-        ->where(function($q) use ($words) {
-            foreach ($words as $word) {
-                $q->orWhere('name', 'like', '%' . $word . '%');
-            }
-        })
+        ->where('name', 'like', '%' . $keyword . '%') // full phrase search
+        ->take(4)
         ->get();
 
     $trendingProducts = $trendingProducts->merge($matched);
