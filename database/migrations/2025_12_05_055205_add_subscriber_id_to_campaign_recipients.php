3a3b88fc35023ef27d0592ce9cf04540ffaac4d5
HashimKhan923 <khanhash1994@gmail.com>
2025-12-05 18:11:41 +0500

    wdwdwdexswd

--- database/migrations/2025_12_05_055205_add_subscriber_id_to_campaign_recipients.php
+++ database/migrations/2025_12_05_055205_add_subscriber_id_to_campaign_recipients.php
@@ -12,9 +12,28 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('campaign_recipients', function (Blueprint $table) {
-            $table->bigInteger('subscriber_id')->unsigned();
-            $table->foreign('subscriber_id')->references('id')->on('subscribers')->onDelete('cascade')->onUpdate('cascade');
+
+            // Drop old unique index
+            $table->dropUnique('campaign_recipients_campaign_id_user_id_unique');
+
+            // Make user_id nullable
+            $table->dropForeign(['user_id']);
+            $table->unsignedBigInteger('user_id')->nullable()->change();
+            $table->foreign('user_id')
+                ->references('id')->on('users')
+                ->onDelete('cascade');
+
+            // Add subscriber_id nullable
+            $table->unsignedBigInteger('subscriber_id')->nullable()->after('user_id');
+            $table->foreign('subscriber_id')
+                ->references('id')->on('subscribers')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+
+            // New combined unique index
+            $table->unique(['campaign_id', 'user_id', 'subscriber_id'], 'campaign_user_subscriber_unique');
         });
+
     }
 
     /**
